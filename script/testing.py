3e7a74196cd947bee653aa7a068c8962f66f6f51
miguelfdz <miguelfdzm08@gmail.com>
2021-03-22 10:02:14 -0600

    Added condition for error 404

--- script/testing.py
+++ script/testing.py
@@ -1,30 +1,36 @@
 #https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Confirmados_20210317.csv
-import datetime
+from datetime import timedelta, date, datetime
 import requests
 
 def urlNueva():
 	#Metodo que obtiene el nombre y url del archivo con los datos mas recientes
 	urlCovid= "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Confirmados_"
-	dt = datetime.date.today()
-	fecha = str(dt)
-	fecha = fecha.replace('-', '')
-	return urlCovid+fecha+".csv"
+	diaActual = str(date.today())
+	diaActual = diaAnterior.replace('-', '')
+	return urlCovid+diaAnterior+".csv"
 
-def is_downloadable(url):
-	#Metodo para revisar que tipo de archivo se va a descargar (Validacion)
-	h = requests.head(url, allow_redirects=True)
-	header = h.headers
-	content_type = header.get('content-type')
-	#Solo descargar si el archivo es CSV
-	if 'text/csv' in content_type.lower():
+def urlDiaAnterior():
+    urlCovid= "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Confirmados_"
+    diaAnterior = str(date.today() - timedelta(days=1))
+    diaAnterior = diaAnterior.replace('-', '')
+    return urlCovid+diaAnterior+".csv"
+
+def is_downloadable(status_code):
+	if status_code == 404:
+		return False
+	else:
 		return True
-	return False
 
 def descargarArchivo():
 	#Si el archivo es correcto se descarga
 	url = urlNueva()
-	if is_downloadable(url):
+	r = requests.get(url, allow_redirects=True)
+	status_code = r.status_code
+	if status_code == 404:
+		url = urlDiaAnterior()
 		r = requests.get(url, allow_redirects=True)
+		open('covid.csv', 'wb').write(r.content)
+	else:
 		#Descargar archivo en el directorio actual con el nombre de covid.csv
 		open('covid.csv', 'wb').write(r.content)
 
