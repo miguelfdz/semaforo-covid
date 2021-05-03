f5a413600a43becbbfefa8ee1453d0098bb777c0
miguelfdz <miguelfdzm08@gmail.com>
2021-05-03 09:10:37 -0500

    renamed files and population variable

--- script/csv_reader.py
+++ script/csv_reader.py
@@ -3,6 +3,7 @@ summatory = 0
 avg = 0.0
 daily_cases = 0.0
 status = ""
+nl_population = 5784000
 
 with open('covid.csv', mode='r') as csv_file:
   csv_reader = csv.DictReader(csv_file)
@@ -12,7 +13,7 @@ with open('covid.csv', mode='r') as csv_file:
       for x in list(reversed(list(row)))[0:7]:
         summatory += int(row[x])
       avg = summatory/7
-      daily_cases = (avg/5784000)*100000
+      daily_cases = (avg/nl_population)*100000
 
 def get_nl_status(daily_cases):
   if daily_cases < 0.5:
